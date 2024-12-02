90eb37e95a9354f5d8560e637c073664cc56fa1e
Danielle Badobre <badobredanielle@gmail.com>
2024-12-02 02:16:32 +0000

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,11 +8,13 @@ determine the fewest number of coins needed to meet a given amount total.
 def makeChange(coins, total):
     if total <= 0:
         return 0
+    coins.sort(reverse=True)
     dp_array = [float('inf')] * (total + 1)
     dp_array[0] = 0
 
     for coin in coins:
         for x in range(coin, total + 1):
-            dp_array[x] = min(dp_array[x], dp_array[x - coin] + 1)
+            if dp_array[x - coin] + 1 < dp_array[x]:
+                dp_array[x] = dp_array[x - coin] + 1
 
     return dp_array[total] if dp_array[total] != float('inf') else -1
